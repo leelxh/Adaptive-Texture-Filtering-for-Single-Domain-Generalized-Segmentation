e45ebc91516c9badf662ec1c783f51b0f9be969d
leelxh <66929869+leelxh@users.noreply.github.com>
2023-03-18 12:10:27 +0800

    Add files via upload

---
+++ texture_filter/util/loss.py
@@ -0,0 +1,37 @@
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torchvision import models
+
+def cross_entropy_loss2d(prediction, label):
+    label = label.long()
+    mask = (label != 0).float()
+    num_positive = torch.sum(mask).float()
+    num_negative = mask.numel() - num_positive
+    mask[mask != 0] = num_negative / (num_positive + num_negative)
+    mask[mask == 0] = num_positive / (num_positive + num_negative)
+    cost = torch.nn.functional.binary_cross_entropy_with_logits(
+            prediction, label, weight=mask, size_average=True)
+    return cost
+
+
+def cross_entropy2d(input, target, weight=None, size_average=True):
+    n, c, h, w = input.size()
+    log_p = F.log_softmax(input, dim=1)
+    log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
+    log_p = log_p[target.view(n * h * w, 1).repeat(1, c) >= 0]
+    log_p = log_p.view(-1, c)
+
+    mask = target >= 0
+    target = target[mask]
+    loss = F.nll_loss(log_p, target, ignore_index=255,
+                      weight=weight, size_average=False)
+    if size_average:
+        loss /= mask.data.sum().float()
+    return loss
+
+def myL1Loss(source, target):
+    return torch.mean(torch.abs(source - target))
+
